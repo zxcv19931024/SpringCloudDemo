b706d5361afcab5f2a63a40b69de745d308d9b10
47207 <472075330@qq.com>
2021-05-27 15:05:35 +0800

    444

--- config-server/src/main/java/com/yun/ConfigServerApplication.java
+++ config-server/src/main/java/com/yun/ConfigServerApplication.java
@@ -14,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class ConfigServerApplication {
     public static void main(String[] args) {
         int port = 8030;
+
         if(!NetUtil.isUsableLocalPort(port)) {
             System.err.printf("端口%d被占用了，无法启动%n", port );
             System.exit(1);
